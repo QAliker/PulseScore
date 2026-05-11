667213c1dda5ab11b3ee7225ae08479445de0c5d
Quentin <qaliker@lequipe.fr>
2026-05-11 14:12:41 +0200

    fix config

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -15,6 +15,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: '*.api-sports.io',
       },
+      {
+        protocol: 'https',
+        hostname: 'crests.football-data.org',
+      },
     ],
   },
 };
